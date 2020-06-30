793143a60d69c52de374be880f5f93f8ebb6b39c
keeykeey <sh1n_fuj1to@icloud.com>
2020-06-30 23:45:03 +0900

    add crowling code to a download.py

--- download.py
+++ download.py
@@ -1,19 +1,36 @@
-from flicrapi import FlickrAPI
+from flickrapi import FlickrAPI
 from urllib.request import urlretrieve
 from pprint import pprint
 import os,time,sys
 
-key = :
+#info about API_key
+KEY = os.environ['ANIMALCLASSIFIER_KEY']
+SECRET_KEY = os.environ['ANIMALCLASSIFIER_SECRET_KEY']
+WAITTIME = 1
 
+#save directories
+animalName = sys.argv[1]
+saveDir = "./" + animalName
 
+flickr = FlickrAPI(KEY,SECRET_KEY,format = 'parsed-json')
+result = flickr.photos.search(
+    text = animalName,
+    per_page = 400,
+    media = 'photos',
+    sort = 'relevance',
+    safe_search = 1,
+    extras = 'url_q, licence'
+)
 
+photos = result['photos']
 
+#pprint(photos)
 
-
-
-
-
-
-
+for i,photo in enumerate(photos['photo']):
+    url_q = photo['url_q']
+    filepath = saveDir + '/' + photo['id'] + '.jpg'
+    if os.path.exists(filepath):continue
+    urlretrieve(url_q,filepath)
+    time.sleep(WAITTIME)
 
 
