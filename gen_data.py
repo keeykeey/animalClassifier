bf3f151832e94dc40e75ed55abb69aa1d5acf56a
keeykeey <sh1n_fuj1to@icloud.com>
2020-07-03 23:48:44 +0900

    write gen_data

--- gen_data.py
+++ gen_data.py
@@ -7,4 +7,15 @@ classes = ['monkey','boar','crow']
 num_classes = len(classes)
 image_size = 50
 
+#load the data
 
+X = []
+Y = []
+for index,class in enumerate(classes):
+    photos_dir = "./" + class
+    files = glob.glob(photos_dir + '/*.jpg')
+    for i,file in enumerate(files):
+        if i >= 200:break
+        image = Image.open(file)
+        image = Image.convert('RGB')
+        image = Image.resize(image_size)
