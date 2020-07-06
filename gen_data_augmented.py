f9115a2e0b0b89f29b0067272bfff1d9ec770551
keeykeey <sh1n_fuj1to@icloud.com>
2020-07-06 23:41:18 +0900

    write code to augment datas

---
+++ gen_data_augmented.py
@@ -0,0 +1,53 @@
+from PIL import Image
+import os,glob
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+classes = ['monkey','boar','crow']
+num_classes = len(classes)
+image_size = 50
+num_testdata = 100
+
+#load the data
+
+X_train = []
+X_test = []
+Y_train = []
+Y_test = []
+
+for index,classLabel in enumerate(classes):
+    photos_dir = "./" + classLabel
+    files = glob.glob(photos_dir + '/*.jpg')
+    for i,file in enumerate(files):
+        if i >= 200:break
+        image = Image.open(file)
+        image = image.convert('RGB')
+        image = image.resize((image_size,image_size))
+        data = np.asarray(image)
+
+        if i < num_testdata:
+            X_test.append(data)
+            Y_test.append(index)
+        else:
+            X_train.append(data)
+            Y_train.append(data)
+          
+            for angle in range(-20,20,5):
+                img_rotate = image.rotate(angle)
+                data = np.asarray(img_rotate)
+                X_train.append(data)
+                Y_train.append(index)
+
+                img_trans = image.transpose(Image.FLIP_LEFT_RIGHT)
+                data = np.asarray(img_trans) 
+                X_train.append(data)
+                Y_train.append(index)
+       
+X_train = np.array(X_train)
+X_test = np.array(X_test)
+Y_train = np.array(Y_train)
+Y_test = np.array(Y_test)
+
+xy = (X_train,X_test,Y_train,Y_test)
+np.save('./animal_aug.npy',xy)
+
