c54916d481bd92e5b34e5d1aceab04cde0efe88a
keeykeey <sh1n_fuj1to@icloud.com>
2020-07-18 14:30:48 +0900

    modified predict.py

--- predict.py
+++ predict.py
@@ -1,7 +1,5 @@
-from keras.models import Sequential,load_model
-from keras.layers import Conv2d,MaxPooling2D
-from keras.layers import Activation,Dropout,Flatten,Dense
-from keras.utils import np_utils
+from keras.models import load_model
+from keras.layers import Conv2D
 import keras
 import sys
 import numpy as np
@@ -12,42 +10,15 @@ num_classes = len(classes)
 image_size = 50
 
 def buil_model():
-    model = Sequential()
-    model.add(Conv2D(32,(3,3),padding = 'same',input_size=(50,50,3)))
-    model.add(Activation('relu'))
-    model.add(Conv2D(32,(3,3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size = (2,2)))
-    model.add(Dropout(0.25))
 
-    model.add(Conv2D(64,(3,3),padding='same'))
-    model.add(Actication('relu'))
-    model.add(Conv2D(64,(3,3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2,2)))
-    model.add(Dropout(0.25))
-
-    model.add(Flatten())
-    model.add(Dense(512))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(3))
-    model.add(Activation('softmax'))
-
-    opt = keras.optimizers.rmsprop(lr=0.001,decay=1e-6)
-
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=opt, 
-                  metrics=['accuracy'])
-
-    model = model.load('./animal_cnn_aug_Mon Jul 13 00:47:39 2020.h')
+    model = load_model('./models/animal_cnn_aug_Mon Jul 13 00:47:39 2020.h')
                    
     return model
 
 def main():
     image = Image.open(sys.argv[1])
-    image = Image.comvert('RGB')
-    image = image.resize((image_size))
+    image = image.convert('RGB')
+    image = image.resize((image_size,image_size))
     data = np.asarray(image)
     X = []
     X.append(data)
@@ -61,5 +32,3 @@ def main():
     
 if __name__ == "__main__":
     main()
-
-
