ea9a9285269ca8b6e0952a93daeb1445ead9f473
keeykeey <sh1n_fuj1to@icloud.com>
2020-07-29 00:54:01 +0900

    edit js script

---
+++ static/js/getImageFromWeb.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    var canpas = document.getElementById('mycanvas');
+    var ctx = canvas.getContext('2d');
+    var image = new.Image();
+    img.onload = function(){
+        ctx.drawImage(img,0,0);
+    };
+    img.src = './dog.jpg':
