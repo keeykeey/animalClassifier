13c70f7f49677d88cb35bd87626d0ff05d8bbe10
keeykeey <sh1n_fuj1to@icloud.com>
2020-08-18 01:07:09 +0900

    edited

---
+++ downloadAndSendToGCS.py
@@ -0,0 +1,43 @@
+from flickrapi import FlickrAPI
+from urllib.request import urlretrieve
+from pprint import pprint
+import os,time,sys
+
+#info about API_key
+KEY = os.environ['ANIMALCLASSIFIER_KEY']
+SECRET_KEY = os.environ['ANIMALCLASSIFIER_SECRET_KEY']
+WAITTIME = 1
+
+#save directories
+animalName = sys.argv[1]
+saveDir = "./" + animalName
+
+flickr = FlickrAPI(KEY,SECRET_KEY,format = 'parsed-json')
+result = flickr.photos.search(
+    text = animalName,
+    per_page = 400,
+    media = 'photos',
+    sort = 'relevance',
+    safe_search = 1,
+    extras = 'url_q, licence'
+)
+
+photos = result['photos']
+
+#save in local environment
+'''
+for i,photo in enumerate(photos['photo']):
+    url_q = photo['url_q']
+    filepath = saveDir + '/' + photo['id'] + '.jpg'
+    if os.path.exists(filepath):continue
+    urlretrieve(url_q,filepath)
+    time.sleep(WAITTIME)
+'''
+
+#send to google cloud storae
+cloudStorageBucketName = 'real_or_picture'
+for i,photo in enumerate(photos['photo']):
+    url_q = photo['url_q']
+    filepath = 
+
+
