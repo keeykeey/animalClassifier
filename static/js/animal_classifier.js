acf5943870752ec7d8f0e56a5c13e63835336c65
keeykeey <sh1n_fuj1to@icloud.com>
2020-08-28 00:35:33 +0900

    edited

--- static/js/animal_classifier.js
+++ static/js/animal_classifier.js
@@ -1,37 +1,6 @@
-/*
-const uploadedPicture = document.querySelector('.uploaded');
-
-uploadedPicture.addEventListener('dragstart',() =>{
-    console.log('dragstart event');
-});
-
-uploadedPicture.addEventListener('drag',() => {
-    console.log('drag event');
-});
-
-uploadedPicture.addEventListener('dragend',() => {
-    console.log('dragend event');
-});    
-*/
-
-/*
-const fileZone = document.querySelector('.file-zone');
-const className = 'on';
-
-fileZone.addEventListener('dragover', (event) => {                         
-    event.preventDefault();                                                  
-    fileZone.classList.add(className);                                       
-    console.log('oh')
-});                                                                        
-                                                                            
-fileZone.addEventListener('dragleave', (event) => {                             
-    event.preventDefault();                                                  
-    fileZone.classList.remove(className);                                    
-    console.log('aha')
-});
-*/
-
 const fileZone=document.querySelector('.file-zone');
+const previewArea = document.querySelector('.file-preview-area');
+const uploadByButton = document.getElementById('upload-image-by-button');
 const className = 'on';
 
 
@@ -77,4 +46,8 @@ function displayImages(transferdFiles){
         });
     }
 }
+
+
+
+
 
